38ed868d69b2d912dbbee891b2e4d94cdec979bd
TasmiyaNazMakandar <77617476+TasmiyaNazMakandar@users.noreply.github.com>
2021-01-26 08:55:28 +0530

    Update Login_Screen.dart

--- Login_Screen.dart
+++ Login_Screen.dart
@@ -1,25 +1,47 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:heath_care/bim-cal.dart';
 import 'package:heath_care/homepage.dart';
 import 'package:heath_care/reg.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 class LoginPage extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() => new _State();
 }
 
 class _State extends State<LoginPage> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+  @override
+
+  final nameController = TextEditingController();
+  final passwordController = TextEditingController();
+  SharedPreferences logindata;
+  bool newuser;
+  @override
+  void initState() {
+    super.initState();
+    check_if_already_login();
+  }
+  void check_if_already_login() async {
+    logindata = await SharedPreferences.getInstance();
+    newuser = (logindata.getBool('login') ?? true);
+    print(newuser);
+    if (newuser == false) {
+      Navigator.pushReplacement(
+          context, new MaterialPageRoute(builder: (context) => Myhome()));
+    }
+  }
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    nameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
     void setpass() {
       setState(() {
 
-
-      if(nameController.text=='tas' && passwordController.text=='123')
+        if(nameController.text=='tas' && passwordController.text=='123')
         {
           return showDialog(
             context: context,
@@ -32,31 +54,34 @@ class _State extends State<LoginPage> {
 
                 ],
               ),
-              //Text('Alert Dialogbox '),
+
 
               content: Text('Succeful'),
+
 
               actions: <Widget> [
                 FlatButton(
                   child: Text('ok'),
+
                   onPressed: () {
+                    logindata.setBool('login', false);
+                    logindata.setString("username", "tas");
+                    logindata.setString("password", "123");
                     Navigator.push(context,
                         MaterialPageRoute(builder: (context) => Myhome()));
-                  },
+                    },
+
+
 
                 )
               ],
             )
         );
 
-
-
         }
       else {
 
-
-
-              return showDialog(
+        return showDialog(
                   context: context,
                   builder: (cxt)=> AlertDialog(
 
@@ -75,9 +100,13 @@ class _State extends State<LoginPage> {
                       FlatButton(
                         child: Text('ok'),
                         onPressed: () {
+                          logindata.setBool('login', false);
+                          logindata.setString("username", "tas");
+                          logindata.setString("password", "123");
                           Navigator.push(context,
                               MaterialPageRoute(builder: (context) => LoginPage()));
                         },
+
 
                       )
                     ],
@@ -92,7 +121,7 @@ class _State extends State<LoginPage> {
     return Scaffold(
         appBar: AppBar(
           title: Text('Health care App '),
-          backgroundColor: Colors.grey,
+          backgroundColor: Colors.blue,
         ),
         body: Container(
           decoration: BoxDecoration(
@@ -104,7 +133,7 @@ class _State extends State<LoginPage> {
             )
           ),
 
-            padding: EdgeInsets.all(10),
+            padding: EdgeInsets.all(8),
 
             child: ListView(
               children: <Widget>[
@@ -112,7 +141,7 @@ class _State extends State<LoginPage> {
                   child: Image.network('https://i.pinimg.com/originals/59/3d/3c/593d3c70fcd3eb601fa6cb39c4f79d50.jpg',width: 80,height: 80),
                 ),
                 Container(
-                    padding: EdgeInsets.all(10),
+                    padding: EdgeInsets.all(8),
                     alignment: Alignment.center,
                     child: Text(
                       'Login Page',
@@ -122,8 +151,9 @@ class _State extends State<LoginPage> {
                           fontSize: 30),
                     )
               ),
+
                 Container(
-                  padding: EdgeInsets.all(10),
+                  padding: EdgeInsets.all(8),
                   child: TextField(
                     controller: nameController,
                     decoration: InputDecoration(
@@ -133,7 +163,7 @@ class _State extends State<LoginPage> {
                   ),
                 ),
                 Container(
-                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
                   child: TextField(
                     obscureText: true,
                     controller: passwordController,
@@ -152,11 +182,18 @@ class _State extends State<LoginPage> {
                 ),
                 Container(
                     height: 50,
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    padding: EdgeInsets.fromLTRB(10, 10, 0,0),
                     child: FloatingActionButton(
 
+
                       child: Text('Login'),
-                      onPressed : setpass,
+
+                      onPressed :(){
+                        setpass();
+                        Navigator.of(context ).pop();
+
+                      },
+
 
                     )
                 ),
