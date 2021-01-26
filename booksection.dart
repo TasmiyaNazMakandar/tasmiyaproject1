1db5a3520afbda717b3437d95df5b89e299c373a
TasmiyaNazMakandar <77617476+TasmiyaNazMakandar@users.noreply.github.com>
2021-01-26 16:27:24 +0530

    Update booksection.dart

--- booksection.dart
+++ booksection.dart
@@ -11,14 +11,23 @@ class _booksectionState extends State<booksection> {
     return Scaffold(
       appBar: AppBar(
         title: (Text('Health Care App')),
-        backgroundColor: Colors.grey,
+        backgroundColor: Colors.blue,
       ),
-      body: Column(
-        children: [
-          Image.network('https://media.tenor.com/images/63487eced92f98b84cb301de62c0fb46/tenor.gif'),
-          Image.network('https://media1.giphy.com/media/l46Cei9gnXaJFGInu/giphy.gif'),
-          Image.network('https://i.pinimg.com/originals/a7/78/19/a778193d04758b169fca966fe0655645.gif' , width: 210,height: 215,),
-        ],
+      body: Container(
+        color: Colors.black12,
+        width: MediaQuery.of(context).size.width*0.8,
+        height: MediaQuery.of(context).size.height*0.8,
+        child: Column(
+          children: [
+
+            Image.network('https://media.tenor.com/images/63487eced92f98b84cb301de62c0fb46/tenor.gif',width: 100,height: 100,),
+            Image.network('https://media1.giphy.com/media/l46Cei9gnXaJFGInu/giphy.gif',width: 100,),
+            Image.network('https://i.pinimg.com/originals/a7/78/19/a778193d04758b169fca966fe0655645.gif' , width: 100,height: 100,),
+
+
+          ],
+        ),
+
       ),
     );
   }
