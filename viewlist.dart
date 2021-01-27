8c1bcc7077c4196e353d449c398658cfebd72df9
TasmiyaNazMakandar <77617476+TasmiyaNazMakandar@users.noreply.github.com>
2021-01-27 11:30:51 +0530

    Update viewlist.dart

--- viewlist.dart
+++ viewlist.dart
@@ -10,173 +10,203 @@ import 'vikas.dart';
 
 
 class View extends StatelessWidget {
+  TextEditingController controller = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Health care App',style: TextStyle(
-            color: Colors.white,
+          title: Text('Find Your Desired Doctor',style: TextStyle(
+            color: Colors.black,
                 fontWeight: FontWeight.bold
           ),
           ),
-          backgroundColor: Colors.blue,
+          backgroundColor: Colors.white,
         ),
-    body: ListSearch(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+
+          child: ListView(
+
+            children: [
+              Column(
+                children: [
+                  Container(
+                    width:650,
+                    height: 30,
+                    color: Colors.white,
+                    child: ListTile(
+                      leading: Icon(Icons.search_rounded,color: Colors.orangeAccent,),
+                      title: TextField(
+                        controller: controller,
+                        decoration: InputDecoration(
+                            hintText: 'Search for doctor',border: InputBorder.none
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+
+                    margin: const EdgeInsets.all(10),
+                    width: 250,
+                    height: 30,
+                    child: Text('Categoris',style: TextStyle(
+                        fontWeight: FontWeight.bold
+                    ),
+                    ),
+                  ),
+
+                  Container(
+
+                    child: Card(
+                      elevation: 3,
+                      color: Colors.white,
+                      child: Row(
+                        children: [
+                          FractionalTranslation(
+                            translation: Offset(0.5, -0.3),
+                            child: Align(
+                              child: Container(
+                                width: 30,
+                                height: 40,
+                                child: Image.network('https://image.shutterstock.com/image-vector/cartoon-tooth-giving-thumb-260nw-602566124.jpg',width: 400,height: 300,),
+                                decoration: BoxDecoration(
+                                    color: Colors.blue,
+                                    borderRadius: BorderRadius.circular(10)
+                                ) ,
+                                padding: const EdgeInsets.only(left: 0),
+
+                              ),
+                            ),
+                          ),
+                          Container(
+                            child: Text("Dental \nSurgeon",style: TextStyle(color: Colors.purple),),
+                            padding: const EdgeInsets.only(top: 0, right: 10, left: 10),
+                            margin: const EdgeInsets.only(top: 0, right: 10, left: 10),
+                          ),
+                          Card(
+                            child:Row(
+                              children: [
+                                FractionalTranslation(
+                                  translation: Offset(0.5, -0.3),
+                                  child: Align(
+                                    child: Container(
+                                      width: 30,
+                                      height: 40,
+                                      child: Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXoa59T6l1WBkC-PMA3rXwtKfC-n4om1hd0Q&usqp=CAU',width: 400,height: 300,),
+                                      decoration: BoxDecoration(
+                                          color: Colors.blue,
+                                          borderRadius: BorderRadius.circular(10)
+                                      ) ,
+                                      padding: const EdgeInsets.only(left: 0),
+
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  child: Text("Heart \nSurgeon",style: TextStyle(color: Colors.purple),),
+                                  padding: const EdgeInsets.only(top: 0, right: 20, left: 10),
+                                  margin: const EdgeInsets.only(top: 0, right: 20, left: 10),
+                                ),
 
 
-    );
-  }
-}
-class ListSearch extends StatefulWidget {
-  ListSearchState createState() => ListSearchState();
-}
+                              ],
+                            ),
 
-class ListSearchState extends State<ListSearch> {
 
-  TextEditingController _textController = TextEditingController();
+                          ),
 
-  static List<String> mainDataList = [
+                        ],
+                      ),
+                    ),
+                  )
 
-  ];
 
-  // Copy Main List into New List.
-  List<String> newDataList = List.from(mainDataList);
-
-  onItemChanged(String value) {
-    setState(() {
-      newDataList = mainDataList
-          .where((string) => string.toLowerCase().contains(value.toLowerCase()))
-          .toList();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor:Colors.white,body: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
-        children: <Widget>[
-
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: TextField(
-              controller: _textController,
-              decoration: InputDecoration(
-                hintText: 'Search Here...',
+                ],
               ),
-              onChanged: onItemChanged,
-            ),
+              Container(
+                height: 30,
+                child:Text('Top Doctors',textAlign: TextAlign.left,style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),) ,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Drvikas()));
+                },
+                child: Container(
+                  width: 330,
+                  height: 90,
+                  color: Colors.blue,
+                  child: ListTile(
+                    leading: Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaXts5ABhK9e_f6JHZ1B0-vidUBb75LTOSYw&usqp=CAU'),
+                    title: Text('Dr.Vikas'),
+                    subtitle: Text('Heart Surgon-Flower Hospitals'),
+                  ),
+
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>A()));
+                },
+                child: Container(
+                  width: 330,
+                  height: 90,
+                  color: Colors.white24,
+                  child: ListTile(
+                    leading: Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaXts5ABhK9e_f6JHZ1B0-vidUBb75LTOSYw&usqp=CAU'),
+                    title: Text('Dr.Shruti'),
+                    subtitle: Text('Dental Surgon-Flower Hospitals'),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Amulya()));
+                },
+                child: Container(
+                  width: 330,
+                  height: 90,
+                  color: Colors.blue,
+                  child: ListTile(
+                    leading: Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaXts5ABhK9e_f6JHZ1B0-vidUBb75LTOSYw&usqp=CAU'),
+                    title: Text('Dr.Amuliya'),
+                    subtitle: Text('Heart Surgon-Flower Hospitals'),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>AA2()));
+                },
+                child: Container(
+                  width: 330,
+                  height: 90,
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaXts5ABhK9e_f6JHZ1B0-vidUBb75LTOSYw&usqp=CAU'),
+                    title: Text('Dr.Arbaaz'),
+                    subtitle: Text('Dental Surgon-Flower Hospitals'),
+                  ),
+                ),
+              )
+
+
+
+
+
+
+            ],
+
+
           ),
-          Expanded(
-            child: ListView(
-              padding: EdgeInsets.all(12.0),
-
-              children: [
-               Text('Top Doctors ',textAlign: TextAlign.start,style: TextStyle(
-                 fontWeight: FontWeight.bold
-               ),
-
-               ),
-
-                Column(
-                  children: [
-                    Container(
-                      width: 270,
-                      height:40,
-                      color: Colors.white,
-                      child: Text('Find Your Desired Doctor',textAlign: TextAlign.center,style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black
-                      ),),
-                    ),
-
-                    Container(
-                      width: 370,
-                      height:110,
-                      color: Colors.blue,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        RaisedButton(onPressed:(){
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => Drvikas()));
-                        },
-                        child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgS_ewjAIkhoGGOm_947G1QW1tHbexcAen6w&usqp=CAU",width:100,height: 100,),
-                        ),
-                        Text('Dr.Vikas',textAlign: TextAlign.justify,style: TextStyle(
-                          fontWeight: FontWeight.bold
-                        ),
-                        ),
-
-                      ],
-                    ),
-                    ),
-                    Container(
-                      width: 370,
-                      height:110,
-                      color: Colors.white,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          RaisedButton(onPressed:(){Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => Amulya()));},
-                            child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmc-cXYXofN9hPuX5omSPDrEY45YLWuvHrng&usqp=CAU",width:100,height: 100,),
-                          ),
-                          Text('Dr.Amulya',textAlign: TextAlign.justify,style: TextStyle(
-                              fontWeight: FontWeight.bold
-                          ),
-                          ),
-
-                        ],
-                      ),
-                    ),
-                    Container(
-                      width: 370,
-                      height:110,
-                      color: Colors.blue,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          RaisedButton(onPressed:(){Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => AA2()));},
-                            child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6vvtZydhOANrVMNrnSTKxHZirhWvY9R6i1g&usqp=CAU",width:100,height: 100,),
-                          ),
-                          Text('Dr.Arbaaz',textAlign: TextAlign.justify,style: TextStyle(
-                              fontWeight: FontWeight.bold
-                          ),
-                          ),
-
-                        ],
-                      ),
-                    ),
-                    Container(
-                      width: 370,
-                      height:110,
-                      color: Colors.white,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          RaisedButton(onPressed:(){Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => A()));},
-                            child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSV_RPN2bROTCOrnw6M41cC-P6Rm9eu4mDTyA&usqp=CAU",width:100,height: 100,),
-                          ),
-                          Text('Dr.Shruti',textAlign: TextAlign.justify,style: TextStyle(
-                              fontWeight: FontWeight.bold
-                          ),
-                          ),
-
-                        ],
-                      ),
-                    ),
-                  ],
-
-                )
-              ],
-            ),
-          )
-        ],
+        ),
       ),
+
+
+
+
     );
   }
 }
